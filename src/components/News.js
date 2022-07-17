f1c07f14afded2a3550488597e97df8289a44edb
JahirGithub <jahuruddinsk2000@gmail.com>
2022-07-17 22:17:51 +0530

    updated code

--- src/components/News.js
+++ src/components/News.js
@@ -68,15 +68,15 @@ export class News extends Component {
                 <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>Daily News - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner />}
                 <InfiniteScroll
-                    dataLength={this.state.articles.length}
+                    dataLength={this.state.articles?.length}
                     next={this.fetchMoreData}
-                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    hasMore={this.state.articles?.length !== this.state.totalResults}
                     loader={<Spinner/>}
                 > 
                     <div className="container">
                          
                     <div className="row">
-                        {this.state.articles.map((element) => {
+                        {this.state.articles?.map((element) => {
                             return <div className="col-md-4" key={element.url}>
                                 <NewsItems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                             </div>
